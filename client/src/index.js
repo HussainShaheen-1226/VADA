0a44ab92eee1daf52b267a91b0aceb8513341bcb
HussainShaheen-1226 <hussainshaheen1226@gmail.com>
2025-08-19 06:31:40 +0500

    Update index.js

--- client/src/index.js
+++ client/src/index.js
@@ -1,18 +1,17 @@
-import React from 'react';
-import { createRoot } from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
-import App from './App';
-import './index.css';
+import React from "react";
+import { createRoot } from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import App from "./App";
+import "./index.css";
 
-createRoot(document.getElementById('root')).render(
+const root = createRoot(document.getElementById("root"));
+root.render(
   <BrowserRouter>
     <App />
   </BrowserRouter>
 );
 
-// Register service worker for Push/PWA
-if ('serviceWorker' in navigator) {
-  navigator.serviceWorker.register('/sw.js').then(() => {
-    console.log('Service Worker registered');
-  });
+// SW
+if ("serviceWorker" in navigator) {
+  navigator.serviceWorker.register("/sw.js");
 }
