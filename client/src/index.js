550bf10018d162ba84eadaab7c05d474269be748
HussainShaheen-1226 <hussainshaheen1226@gmail.com>
2025-08-23 20:45:42 +0500

    Update index.js

--- client/src/index.js
+++ client/src/index.js
@@ -1,17 +1,19 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import App from "./App";
 import "./index.css";
 
 const root = createRoot(document.getElementById("root"));
 root.render(
-  <BrowserRouter>
+  <HashRouter>
     <App />
-  </BrowserRouter>
+  </HashRouter>
 );
 
-// SW
-if ("serviceWorker" in navigator) {
-  navigator.serviceWorker.register("/sw.js");
+// Kill stale service workers that can cause white-screen
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker.getRegistrations()
+    .then(regs => regs.forEach(r => r.unregister()))
+    .catch(() => {});
 }
